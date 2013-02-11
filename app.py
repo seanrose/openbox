0e2f804cc0443d6a36b5f48d70aedaa78b984157
Sean Rose <sean.andrew.rose@gmail.com>
2013-02-10 22:45:27 -0800

    Removed secret key and session handling

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import os
 import requests
 import simplejson as json
-
 from flask import Flask, request, render_template
-from settings import SECRET_KEY
-
-# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 app = Flask(__name__)
 
@@ -52,5 +48,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.debug = True
-    app.secret_key = SECRET_KEY
     app.run(host='0.0.0.0', port=port)
