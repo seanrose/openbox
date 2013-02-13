0cebfeaf0d216390487951178477950c3c18df93
Sean Rose <sean.andrew.rose@gmail.com>
2013-02-12 19:38:17 -0800

    trying out codemirror

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def box_auth():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.debug = False
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
